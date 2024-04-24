55cffd68eebae2ab6fac498ae8b481a09ce08159
Roman Durajka <romo.durajka@gmail.com>
2024-04-24 19:22:20 +0200

    changed indentation (i am bullied)

--- src/utils/file_reader.c
+++ src/utils/file_reader.c
@@ -2,38 +2,38 @@
 #include <stdlib.h>
 
 size_t read_file_size(FILE * f) {
-  fseek(f, 0, SEEK_END);
-  size_t size = ftell(f);
-  fseek(f, 0, SEEK_SET);
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    fseek(f, 0, SEEK_SET);
 
-  return size;
+    return size;
 }
 
 // function returns initialized buffer, don't forget to free
 char * read_plaintext_file(const char *filename, size_t *out_size) {
-  FILE * f = fopen(filename, "r");
-  if (f == NULL) {
-    fprintf(stderr, "Could not read file %s\n", filename);
-    return NULL;
-  }
+    FILE * f = fopen(filename, "r");
+    if (f == NULL) {
+        fprintf(stderr, "Could not read file %s\n", filename);
+        return NULL;
+    }
 
-  int file_size = read_file_size(f);
-  char * buffer = calloc(file_size, sizeof(char));
-  if (buffer == NULL) {
-    fprintf(stderr, "Could not allocate memory\n");
+    int file_size = read_file_size(f);
+    char * buffer = calloc(file_size, sizeof(char));
+    if (buffer == NULL) {
+        fprintf(stderr, "Could not allocate memory\n");
+        fclose(f);
+        return NULL;
+    }
+
+    size_t bytes_read = fread(buffer, 1, file_size, f);
+    if (bytes_read != file_size) {
+        fprintf(stderr, "Could not read all data from file\n");
+        free(buffer);
+        fclose(f);
+        return NULL;
+    }
+
     fclose(f);
-    return NULL;
-  }
-
-  size_t bytes_read = fread(buffer, 1, file_size, f);
-  if (bytes_read != file_size) {
-    fprintf(stderr, "Could not read all data from file\n");
-    free(buffer);
-    fclose(f);
-    return NULL;
-  }
-
-  fclose(f);
-  *out_size = file_size;
-  return buffer;
+    *out_size = file_size;
+    return buffer;
 }
--- tests/utils/file_reader.test.c
+++ tests/utils/file_reader.test.c
@@ -4,46 +4,46 @@
 #include <string.h>
 
 int test_read_file_size() {
-  printf("#########################\n");
-  printf("### test_read_file_size()\n");
-  printf("#########################\n");
+    printf("#########################\n");
+    printf("### test_read_file_size()\n");
+    printf("#########################\n");
 
-  FILE *f = fopen("../tests/utils/foo.txt", "r");
-  size_t size = read_file_size(f);
-  printf("File size: %lu\n", size);
+    FILE *f = fopen("../tests/utils/foo.txt", "r");
+    size_t size = read_file_size(f);
+    printf("File size: %lu\n", size);
 
-  fclose(f);
+    fclose(f);
 
-  if (size != 1274) {
-    printf("File size not matching, aborting\n");
-    exit(1);
-  }
+    if (size != 1274) {
+        printf("File size not matching, aborting\n");
+        exit(1);
+    }
 
-  return 0;
+    return 0;
 }
 
 int test_read_plaintext_file()
 {
-  printf("#########################\n");
-  printf("### test_read_file_size()\n");
-  printf("#########################\n");
+    printf("#########################\n");
+    printf("### test_read_file_size()\n");
+    printf("#########################\n");
 
-  size_t size;
-  char *buffer = read_plaintext_file("../tests/utils/foo.txt", &size);
+    size_t size;
+    char *buffer = read_plaintext_file("../tests/utils/foo.txt", &size);
 
-  if (strncmp(buffer, "I'd just like to interject for a moment", 39) != 0 ||
-      strncmp(buffer + size - 11, "GNU/Linux!", 10) != 0) {
-    printf("Head and tail of text not matching, aborting\n");
-    exit(1);
-  }
-  printf("Head and tail test: OK\n");
+    if (strncmp(buffer, "I'd just like to interject for a moment", 39) != 0 ||
+        strncmp(buffer + size - 11, "GNU/Linux!", 10) != 0) {
+        printf("Head and tail of text not matching, aborting\n");
+        exit(1);
+    }
+    printf("Head and tail test: OK\n");
 
-  return 0;
+    return 0;
 }
 
 int main(){
-  test_read_file_size();
-  test_read_plaintext_file();
+    test_read_file_size();
+    test_read_plaintext_file();
 
-  return 0;
+    return 0;
 }
